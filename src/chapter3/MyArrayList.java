1a1086eeb6d72d8ac7082bd7e8a83ddad38fbf11
Leon Ren <yuren1248@163.com>
2021-04-08 16:23:53 +0800

    Finish MyArrayList and its unit test.

--- src/chapter3/MyArrayList.java
+++ src/chapter3/MyArrayList.java
@@ -1,58 +1,162 @@
 package chapter3;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class MyArrayList<AnyType> implements Iterator<AnyType> {
-    private static final int DEFAULT_CAPACITY = 10;
+public class MyArrayList<AnyType> implements Iterable<AnyType> {
+    private static final int DEFAULT_CAPACITY = 4;
 
     private int theSize;
     private AnyType[] theItems;
 
     public MyArrayList() {
+        clear();
     }
 
     public void clear() {
+        theSize = 0;
+        ensureCapacity(DEFAULT_CAPACITY);
     }
 
     public int size() {
+        return theSize;
     }
 
     public boolean isEmpty() {
+        return theSize == 0;
     }
 
     public void trimToSize() {
+        ensureCapacity(theSize);
     }
 
     public AnyType get(int idx) {
+        if (idx < 0 || idx >= theSize) {
+            throw new IndexOutOfBoundsException();
+        }
+        return theItems[idx];
     }
 
     public AnyType set(int idx, AnyType newVal) {
+        if (idx < 0 || idx >= theSize) {
+            throw new IndexOutOfBoundsException();
+        }
+        AnyType oldValue = theItems[idx];
+        theItems[idx] = newVal;
+        return oldValue;
     }
 
     public void ensureCapacity(int newCapacity) {
+        if (newCapacity < theSize) return;
+        AnyType[] newItems = (AnyType[]) new Object[newCapacity];
+        for (int i = 0; i < theSize; i++) {
+            newItems[i] = theItems[i];
+        }
+        theItems = newItems;
     }
 
     public boolean add(AnyType x) {
+        add(theSize, x);
+        return true;
     }
 
     public void add(int idx, AnyType x) {
+        if (idx < 0 || idx > theSize) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (theSize == theItems.length) {
+            ensureCapacity(2 * theSize);
+        }
+        for (int i = theSize; i > idx; i--) {
+            theItems[i] = theItems[i - 1];
+        }
+        theItems[idx] = x;
+        theSize++;
     }
 
     public AnyType remove(int idx) {
+        if (idx < 0 || idx >= theSize) {
+            throw new IndexOutOfBoundsException();
+        }
+        AnyType oldValue = theItems[idx];
+        for (int i = idx; i < theSize - 1; i++) {
+            theItems[i] = theItems[i + 1];
+        }
+        theSize--;
+        return oldValue;
     }
 
     public Iterator<AnyType> iterator() {
         return new ArrayListIterator();
     }
 
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(theItems, theSize));
+    }
+
     private class ArrayListIterator implements Iterator<AnyType> {
+        int currentIdx = 0;
+
+        @Override
         public boolean hasNext() {
+            return currentIdx < theSize;
         }
 
+        @Override
         public AnyType next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return theItems[currentIdx++];
         }
 
+        @Override
         public void remove() {
+            MyArrayList.this.remove(--currentIdx);
         }
+    }
+
+    // Unit test
+    public static void main(String[] args) {
+        MyArrayList<Double> mal = new MyArrayList<>();
+        int N = 10;
+        for (int i = N - 2; i > 0; i--) {
+            mal.add(i + 0.5);
+        }
+        System.out.println("Initial MyArrayList and basic functions test: " + mal + " Size: " + mal.size());
+
+        mal.clear();
+        for (int i = N; i > 0; i--) {
+            mal.add(i + 0.5);
+        }
+        System.out.println("After clear() test: " + mal + " Size: " + mal.size());
+
+        mal.set(2, 100000000.0);
+        System.out.println("After set() test: " + mal + " Size: " + mal.size());
+
+        System.out.println("After get() test: " + mal + " Size: " + mal.size() + ", Get the 10-th element: " + mal.get(9));
+
+        mal.add(0, 100000000.0);
+        mal.add(100.0);
+        mal.add(1000.0);
+        mal.add(5, 10000.0);
+        System.out.println("After add() test: " + mal + " Size: " + mal.size());
+
+        mal.remove(10);
+        mal.remove(10);
+        System.out.println("After remove() test: " + mal + " Size: " + mal.size());
+
+        // Re-Initialize mal
+        mal.clear();
+        for (int i = N; i > 0; i--) {
+            mal.add(i + 0.5);
+        }
+        double sum = 0;
+        for (double item : mal) {
+            sum += item;
+        }
+        System.out.println("After iterator test: " + mal + " The sum is: " + sum);
     }
 }
